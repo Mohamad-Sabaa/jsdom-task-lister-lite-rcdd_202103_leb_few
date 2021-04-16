ca652bfa8f767f5e174aefe1cd607377d5165f12
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-16 14:52:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-let input = document.getElementById('new-task-description');
+let input = document.getElementById("new-task-description");
 let button = document.getElementById("btnId");
-let toDos = document.getElementById('tasks');
+let toDos = document.getElementById("tasks");
 
   button.addEventListener('click', (e) => {
-    let li = document.creatElement("li");
+    let li = document.createElement("li");
     toDos.appendChild(li);
     li.innerText = input.value;
     e.preventDefault();
