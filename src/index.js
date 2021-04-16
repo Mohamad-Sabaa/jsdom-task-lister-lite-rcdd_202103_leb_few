c0b863a1ca9763c30336f3aa0ed6e3ba610fc1e9
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-16 14:29:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let input = document.getElementById('new-task-description');
+  let toDoList = document.getElementById('tasks');
+  let button = document.getElementById('create-task-form');
+
+  button.addEventListener('click', (e) => {
+    let li = document.createElement('li');
+    li.innerText += input.value;
+    toDoList.appendChild(li);
+    e.preventDefault();
+  })
 });
