46aaefc1c57a62daa5ccb646eeb1ae59eb7b1e2a
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-16 14:44:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let input = document.getElementById("new-task-description");
   let toDos = document.getElementById("tasks");
   let button = document.getElementById("btnId");
-  
+
+  button.addEventListener('click', (e) => {
+    let li = document.createElement('li')
+    toDos.appendChild(li);
+    toDos.innerText = input.value;
+  })
+
+};
+
